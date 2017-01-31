af9ea96b9c93efb9855e20306e4f0099785081d9
Dave Landry <landry.dave@gmail.com>
2017-01-31 10:55:45 -0500

    better promise catching for attr fill

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ axios.get(`${prefix}list`)
 
     const attrs = {};
 
-    // const promises = res.data.data.map(attr => axios.get(`${API}attrs/${attr}`)
-    const promises = ["crop", "geo"].map(attr => axios.get(`${API}attrs/${attr}`)
+    const promises = res.data.data.map(attr => axios.get(`${API}attrs/${attr}`)
       .then(res => {
         console.log(`Cached ${attr} attributes`);
         const data = fold(res.data);
@@ -29,50 +28,58 @@ axios.get(`${prefix}list`)
         return data;
       })
       .catch(err => {
-        console.log(err);
+        console.log(`${API}attrs/d${attr} errored with code ${err.response.status}`);
+        return Promise.reject(err);
       }));
 
-    Promise.all(promises).then(() => {
+    Promise.all(promises)
+      .then(() => {
 
-      const App = require("./static/assets/server");
-      const app = express();
+        console.log("\nInitializing Server");
 
-      if (NODE_ENV === "development") {
-        const webpackDevConfig = require("./webpack/webpack.config.dev-client");
-        const compiler = webpack(webpackDevConfig);
-        app.use(require("webpack-dev-middleware")(compiler, {
-          noInfo: true,
-          publicPath: webpackDevConfig.output.publicPath
+        const App = require("./static/assets/server");
+        const app = express();
+
+        if (NODE_ENV === "development") {
+          const webpackDevConfig = require("./webpack/webpack.config.dev-client");
+          const compiler = webpack(webpackDevConfig);
+          app.use(require("webpack-dev-middleware")(compiler, {
+            noInfo: true,
+            publicPath: webpackDevConfig.output.publicPath
+          }));
+
+          app.use(require("webpack-hot-middleware")(compiler));
+        }
+
+        app.set("port", PORT);
+
+        if (NODE_ENV === "production") {
+          app.use(gzip());
+          app.use(helmet());
+        }
+
+        app.use(express.static(path.join(__dirname, "static")));
+
+        app.set("trust proxy", "loopback");
+
+        console.log("--------------------------");
+        console.log("===>  😊 Starting Server . . .");
+        console.log(`===>  Environment: ${NODE_ENV}`);
+        console.log(`===>  Listening on port: ${PORT}`);
+        console.log("--------------------------");
+
+        app.use(flash());
+
+        app.get("*", App.default({
+          attrs,
+          focus: ["040AF00094", "040AF00253", "040AF00170", "040AF00217", "040AF00042", "040AF00152", "040AF00270", "040AF00257", "040AF00079", "040AF00205", "040AF00182", "040AF00133"]
         }));
+        app.listen(PORT);
 
-        app.use(require("webpack-hot-middleware")(compiler));
-      }
-
-      app.set("port", PORT);
-
-      if (NODE_ENV === "production") {
-        app.use(gzip());
-        app.use(helmet());
-      }
-
-      app.use(express.static(path.join(__dirname, "static")));
-
-      app.set("trust proxy", "loopback");
-
-      console.log("--------------------------");
-      console.log("===>  😊 Starting Server . . .");
-      console.log(`===>  Environment: ${NODE_ENV}`);
-      console.log(`===>  Listening on port: ${PORT}`);
-      console.log("--------------------------");
-
-      app.use(flash());
-
-      app.get("*", App.default({
-        attrs,
-        focus: ["040AF00094", "040AF00253", "040AF00170", "040AF00217", "040AF00042", "040AF00152", "040AF00270", "040AF00257", "040AF00079", "040AF00205", "040AF00182", "040AF00133"]
-      }));
-      app.listen(PORT);
-
-    });
+      })
+      .catch(err => {
+        console.log("Unable to cache attributes, please try again.");
+        return Promise.reject(err);
+      });
 
   });
