ed634ffdbc815d52d9f4a0168fe41f9b5f158d40
Dave Landry <landry.dave@gmail.com>
2017-01-31 10:57:08 -0500

    updates server.js

--- static/assets/server.js
+++ static/assets/server.js
@@ -64654,7 +64654,7 @@ module.exports =
 	  value: true
 	});
 	var config = {
-	  link: [{ rel: "icon", href: "/images/favicon.ico" }],
+	  link: [{ rel: "icon", href: "/images/favicon.ico?v=2" }],
 	  meta: [{ charset: "utf-8" }, { "http-equiv": "X-UA-Compatible", "content": "IE=edge" }, { name: "description", content: "Visualization the open data landscape of Africa." }, { name: "viewport", content: "width=device-width, initial-scale=1" }, { name: "mobile-web-app-capable", content: "yes" }, { name: "apple-mobile-web-app-capable", content: "yes" }, { name: "apple-mobile-web-app-status-bar-style", content: "black" }, { name: "apple-mobile-web-app-title", content: "Data Africa" }]
 	};
 
