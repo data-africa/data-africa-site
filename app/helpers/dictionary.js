d46a63ea147bef7bea296b5b65bb393e7120e698
Jonathan Speiser <jspeis@gmail.com>
2017-01-31 16:53:35 -0500

    adding definitions for poverty

--- app/helpers/dictionary.js
+++ app/helpers/dictionary.js
@@ -1,5 +1,8 @@
 export const DICTIONARY = {
+  hc: "Headcount Ratio",
+  povgap: "Poverty Gap Ratio",
   rainfall_awa_mm: "Average Annual Rainfall",
+  sevpov: "Poverty Severity Ratio",
   stunted: "Stunted",
   underweight: "Underweight",
   wasted: "Wasted"
