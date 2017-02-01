e4737a489ee23336ed76ee84bc05bc05ce13adb9
Alex Simoes <alexandersimoes@users.noreply.github.com>
2017-02-01 12:07:49 -0300

    Minor typo (#1)

--- app/helmconfig.js
+++ app/helmconfig.js
@@ -5,7 +5,7 @@ const config = {
   meta: [
     {charset: "utf-8"},
     {"http-equiv": "X-UA-Compatible", "content": "IE=edge"},
-    {name: "description", content: "Visualization the open data landscape of Africa."},
+    {name: "description", content: "Visualizing the open data landscape of Africa."},
     {name: "viewport", content: "width=device-width, initial-scale=1"},
     {name: "mobile-web-app-capable", content: "yes"},
     {name: "apple-mobile-web-app-capable", content: "yes"},
