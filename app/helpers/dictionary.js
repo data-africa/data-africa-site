7b73f0777d3e347f1090acb892c187a251be020c
Jonathan Speiser <jspeis@gmail.com>
2017-02-07 10:13:35 -0500

    add ppp year to label. closes #5.

--- app/helpers/dictionary.js
+++ app/helpers/dictionary.js
@@ -1,8 +1,8 @@
 export const DICTIONARY = {
   hc: "Headcount Ratio",
   povgap: "Poverty Gap Ratio",
-  ppp1: "$1.90/day (PPP)",
-  ppp2: "$3.10/day (PPP)",
+  ppp1: "$1.90/day (2011 PPP)",
+  ppp2: "$3.10/day (2011 PPP)",
   rainfall_awa_mm: "Average Annual Rainfall",
   sevpov: "Poverty Severity Ratio",
   stunted: "Stunted",
