3d90a7eea17d23b30fd8861bc660ea357d829b9a
Dave Landry <landry.dave@gmail.com>
2017-02-13 11:58:36 -0500

    removed unused compiled files

--- compiled/.env.js
+++
@@ -1,9 +0,0 @@
-function defaultExport() {}
-
-defaultExport.NODE_ENV = process.env.NODE_ENV || "development";
-defaultExport.API = process.env.API || "https://api.dataafrica.io/";
-// defaultExport.API = process.env.API || defaultExport.NODE_ENV === "development"
-//                   ? "http://localhost:5000/" : "https://api.dataafrica.io/";
-defaultExport.PORT = process.env.PORT || 3300;
-
-module.exports = defaultExport;
