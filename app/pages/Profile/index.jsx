81a6216247d0dde965a4563d066287fab153daf1
Jonathan Speiser <jspeis@gmail.com>
2017-02-16 21:55:45 -0500

    topojson settings for adm1

--- app/pages/Profile/index.jsx
+++ app/pages/Profile/index.jsx
@@ -66,7 +66,13 @@ class Profile extends Component {
     const {attrs, focus, stats} = this.props;
     const attr = attrs.geo[id];
     const focusISO = focus.map(f => attrs.geo[f].iso3);
-    const topoPath = attr.level === "adm0" ? "/topojson/continent.json" : "/topojson/cell5m/adm1.json";
+    const isAdm0 = attr.level === "adm0";
+    const topoPath = isAdm0 ? "/topojson/continent.json" : "/topojson/cell5m/adm1.json";
+    const topoFilt = isAdm0 ? d => d : d => adm0 === d.properties.geo.slice(5, 10);
+    const fillAdm0 = d => d.properties.iso_a3 === attr.iso3 ? "white" : focusISO.includes(d.properties.iso_a3) ? "rgba(255, 255, 255, 0.35)" : "rgba(255, 255, 255, 0.1)";
+    const fillAdm1 = d => d.properties.geo === id ? "white" : focusISO.includes(d.properties.iso_a3) ? "rgba(255, 255, 255, 0.35)" : "rgba(255, 255, 255, 0.1)";
+    const fill = isAdm0 ? fillAdm0 : fillAdm1;
+    const adm0 = id.slice(5, 10);
 
     return (
       <div className="profile">
@@ -82,11 +88,12 @@ class Profile extends Component {
               ocean: "transparent",
               padding: 0,
               shapeConfig: {Path: {
-                fill: d => d.properties.iso_a3 === attr.iso3 ? "white" : focusISO.includes(d.properties.iso_a3) ? "rgba(255, 255, 255, 0.35)" : "rgba(255, 255, 255, 0.1)",
+                fill,
                 stroke: "rgba(255, 255, 255, 0.25)"
               }},
               tiles: false,
               topojson: topoPath,
+              topojsonFilter: topoFilt,
               topojsonKey: "collection",
               zoom: false
             }} />
