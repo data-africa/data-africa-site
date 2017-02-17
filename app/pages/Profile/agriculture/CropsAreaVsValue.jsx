7e1c6973fd1e83df3faa0698de561de762344ca1
Jonathan Speiser <jspeis@gmail.com>
2017-02-17 13:01:17 -0500

    pluralize crop names

--- app/pages/Profile/agriculture/CropsAreaVsValue.jsx
+++ app/pages/Profile/agriculture/CropsAreaVsValue.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 import {Plot} from "d3plus-react";
+import pluralize from "pluralize";
 
 import {VARIABLES, FORMATTERS} from "helpers/formatters";
 import {SectionColumns} from "datawheel-canon";
@@ -14,7 +15,7 @@ class CropsAreaVsValue extends Component {
     let crops = vars.harvested_area.slice();
     crops = crops.filter(c => c.harvested_area && c.harvested_area > 0);
     crops.forEach(c => {
-      c.name = attrs[c.crop] ? attrs[c.crop].name : c.crop;
+      c.name = attrs[c.crop] ? pluralize.plural(attrs[c.crop].name) : c.crop;
       c.density = c.value_of_production / c.harvested_area;
     });
     crops.sort((a, b) => b.density - a.density);
@@ -24,8 +25,8 @@ class CropsAreaVsValue extends Component {
     return (
       <SectionColumns title="Harvested Area Versus Value of Production">
       <article>
-        <p><strong>{ topCrop.name }</strong> is the crop with the highest production value per area in { profile.name }, with a harvested area of { VARIABLES.value_density(topCrop.density) }.</p>
-        <p><strong>{ bottomCrop.name }</strong> is the crop with the lowest production value per area in { profile.name }, with a harvested area of { VARIABLES.value_density(bottomCrop.density) }.</p>
+        <p><strong>{ topCrop.name }</strong> are the crop with the highest production value per area in { profile.name }, with a harvested area of { VARIABLES.value_density(topCrop.density) }.</p>
+        <p><strong>{ bottomCrop.name }</strong> are the crop with the lowest production value per area in { profile.name }, with a harvested area of { VARIABLES.value_density(bottomCrop.density) }.</p>
         <p>This means that growers of {topCrop.name} will earn approximately <strong>{FORMATTERS.round(topCrop.density / bottomCrop.density)} times</strong> more per hectacre of {topCrop.name} that they grow versus {bottomCrop.name}.</p>
       </article>
         <Plot config={{
