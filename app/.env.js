26be707288c234b66378542379f516bb35284bb2
Dave Landry <landry.dave@gmail.com>
2017-02-17 14:06:54 -0500

    switches default API to live server

--- app/.env.js
+++ app/.env.js
@@ -4,9 +4,9 @@ defaultExport.NODE_ENV = process.env.NODE_ENV || "development";
 defaultExport.PORT = process.env.PORT || 3300;
 defaultExport.ROOT = __dirname;
 
-defaultExport.API = "http://localhost:5000/";
+defaultExport.API = "https://api.dataafrica.io/";
 // defaultExport.API = process.env.API || defaultExport.NODE_ENV === "development"
-//                   ? "http://localhost:5000/" : "https://api.dataafrica.io/";
+                  // ? "http://localhost:5000/" : "https://api.dataafrica.io/";
 defaultExport.ATTRS = process.env.ATTRS || `${defaultExport.API}attrs/list`;
 // defaultExport.GOOGLE_ANALYTICS = "UA-########-#";
 
