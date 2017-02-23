b272af50e3254b3f5c3850f468620cda3b75aec1
Jonathan Speiser <jspeis@gmail.com>
2017-02-23 12:50:41 -0500

    adding text for rainfall viz

--- app/pages/Profile/climate/RainfallBars.jsx
+++ app/pages/Profile/climate/RainfallBars.jsx
@@ -3,25 +3,30 @@ import {connect} from "react-redux";
 
 import {BarChart} from "d3plus-react";
 import {Section} from "datawheel-canon";
+import {fetchData} from "actions/profile";
+import {VARIABLES} from "helpers/formatters";
 
 import {API} from ".env";
 
 class RainfallBars extends Component {
 
   render() {
-    const {attrs, focus, profile} = this.props;
-    const sumlevel = attrs[profile.id].level;
-    const param = sumlevel === "adm0" ? `geo=${ focus.join(",") }` : `neighbors=${ profile.id }`;
-    const apiUrl = `${API}api/join/?show=geo&${param}&sumlevel=all&required=rainfall_awa_mm`;
-
+    const {focus, profile, data} = this.props;
+    const sumlevel = profile.id.slice(0, 3);
+    const param = sumlevel === "040" ? `geo=${ focus.join(",") }` : `neighbors=${ profile.id }`;
+    const apiUrl = `${API}api/join/?show=geo&${param}&sumlevel=all&required=rainfall_awa_mm&display_names=1`;
+    const res = data.length > 0 ? data[0] : {};
     return (
       <Section title="Rainfall by Location">
+        <article>From {res.start_year} to {res.year} {res.geo_name} had an annual
+        average rainfall of of {VARIABLES.rainfall_awa_mm(res.rainfall_awa_mm)} across a total
+        cropland area of {VARIABLES.harvested_area(res.cropland_total_ha)}.</article>
         <BarChart config={{
           barPadding: 5,
           data: apiUrl,
           discrete: "y",
           groupBy: "geo",
-          label: d => attrs[d.geo] ? attrs[d.geo].name : d.geo,
+          label: d => d.geo_name,
           legend: false,
           shapeConfig: {
             fill: d => d.geo === profile.id ? "rgb(120, 220, 133)" : "rgb(61, 71, 55)"
@@ -41,7 +46,11 @@ class RainfallBars extends Component {
   }
 }
 
+RainfallBars.need = [
+  fetchData("rainfall", "api/join/?show=geo&geo=<id>&sumlevel=all&required=start_year,rainfall_awa_mm,cropland_total_ha&display_names=1")
+];
+
 export default connect(state => ({
-  attrs: state.attrs.geo,
+  data: state.profile.data.rainfall,
   focus: state.focus
 }), {})(RainfallBars);
