24c630ee45d37187c7cb482d43bd2c45965790a3
Dave Landry <landry.dave@gmail.com>
2017-02-27 12:26:19 -0500

    adds color lookup helper file

---
+++ app/helpers/colors.js
@@ -0,0 +1,27 @@
+export const COLORS_CROP = {
+  "Fibres": "#A599D0",
+  "Cereals": "#C993A8",
+  "Fruits": "#DB968E",
+  "Oil Crops": "#D69F7A",
+  "Pulses": "#DBC678",
+  "Roots & Tubers": "#E7E79D",
+  "Stimulants": "#C9DB78",
+  "Sugar Crops": "#AED7A1",
+  "Vegetables": "#809FC2"
+};
+
+export const COLORS_GENDER = {
+  female: "#A599D0",
+  male: "#C9DB78"
+};
+
+export const COLORS_RESIDENCE = {
+  rural: "#AED7A1",
+  urban: "#809FC2"
+};
+
+export const COLORS_CONDITION = {
+  wasted: "#C993A8",
+  stunted: "#D69F7A",
+  underweight: "#DB968E"
+};
