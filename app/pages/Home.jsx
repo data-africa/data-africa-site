61cd34aa6d22305f80d638c77088429ff0077d0b
Dave Landry <landry.dave@gmail.com>
2017-03-01 13:58:31 -0500

    hooks up simple homepage

--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -1,17 +1,49 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import {activateSearch} from "actions/users";
 import "./Home.css";
+
+import {Geomap} from "d3plus-react";
 
 class Home extends Component {
 
   render() {
+
+    const {attrs, focus} = this.props;
+    const focusISO = focus.map(f => attrs.geo[f].iso3);
+
     return (
       <div className="home">
-        <h1 className="title">Coming Soon</h1>
-        <div className="splash"></div>
+        <div className="splash">
+          <div className="image"></div>
+          <div className="gradient"></div>
+        </div>
+        <div className="intro">
+          <div className="text">
+            <h2 className="title">Data Africa is an open source collection of resource data for a growing set of countries</h2>
+            <div className="search-start" onClick={ this.props.activateSearch }>Start A Search</div>
+          </div>
+          <Geomap config={{
+            height: 400,
+            ocean: "transparent",
+            padding: 32,
+            shapeConfig: {Path: {
+              fill: d => focusISO.includes(d.properties.iso_a3) ? "#74E19A" : "rgba(255, 255, 255, 0.35)",
+              stroke: "rgba(255, 255, 255, 0.75)"
+            }},
+            tiles: false,
+            topojson: "/topojson/continent.json",
+            topojsonKey: "collection",
+            width: 400,
+            zoom: false
+          }} />
+        </div>
       </div>
     );
   }
 }
 
-export default connect(() => ({}), {})(Home);
+export default connect(state => ({
+  attrs: state.attrs,
+  focus: state.focus
+}), {activateSearch})(Home);
