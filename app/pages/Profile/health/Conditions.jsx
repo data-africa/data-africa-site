60a4130dbbf0beec8e38b8aadc3f75a4208f55e4
Jonathan Speiser <jspeis@gmail.com>
2017-03-06 13:58:58 -0500

    expanded tooltips for health conditions viz

--- app/pages/Profile/health/Conditions.jsx
+++ app/pages/Profile/health/Conditions.jsx
@@ -9,6 +9,7 @@ import {childHealth} from "pages/Profile/health/shared";
 import {API} from ".env";
 import {COLORS_CONDITION} from "helpers/colors";
 import {FORMATTERS} from "helpers/formatters";
+import {formatPlaceName} from "helpers/formatters";
 
 class Conditions extends SectionColumns {
 
@@ -23,7 +24,7 @@ class Conditions extends SectionColumns {
           {childHealth(profile, dhsHealth)}
         </article>
         <BarChart config={{
-          data: `${API}api/join/?show=condition&geo=${ profile.id }&required=condition,severity,proportion_of_children`,
+          data: `${API}api/join/?show=condition&geo=${ profile.id }&required=dhs_geo_name,dhs_geo_parent_name,condition,severity,proportion_of_children`,
           groupBy: ["condition", "severity"],
           groupPadding: 64,
           label: d => d.condition instanceof Array ? titleCase(d.severity) : `${titleCase(d.severity)}ly ${titleCase(d.condition)}`,
@@ -36,6 +37,9 @@ class Conditions extends SectionColumns {
           stackOrder: series => {
             const order = ["wasted_severe", "stunted_severe", "underweight_severe", "wasted_moderate", "stunted_moderate", "underweight_moderate"];
             return series.map(s => order.indexOf(s.key));
+          },
+          tooltipConfig: {
+            body: d => `Based on data from ${formatPlaceName(d, "health", profile.level)} in ${d.year}`
           },
           x: "year",
           xConfig: {
