341665b2c2a8573148ea1678e3f0eaf6fffc6c1e
Jonathan Speiser <jspeis@gmail.com>
2017-03-13 13:05:34 -0400

    adding dropdown and option class names, closes #42

--- app/pages/Profile/ui/Selector.jsx
+++ app/pages/Profile/ui/Selector.jsx
@@ -3,9 +3,9 @@ import {DICTIONARY} from "helpers/dictionary";
 
 class Selector extends React.Component {
   render() {
-    return <select onChange={this.props.callback}>
+    return <select className="dropdown" onChange={this.props.callback}>
       {this.props.options.map(opt =>
-        <option key={opt.value || opt} value={opt.value || opt}>{DICTIONARY[opt.label || opt] || opt.label}</option>
+        <option className="option" key={opt.value || opt} value={opt.value || opt}>{DICTIONARY[opt.label || opt] || opt.label}</option>
       )}
     </select>;
   }
