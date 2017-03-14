ba834ae9ac990e4c696fbeb0872edaa97a60211f
Dave Landry <landry.dave@gmail.com>
2017-03-14 18:25:14 -0400

    hooks up sticky subnav on scroll (closes #20)

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -6,30 +6,34 @@ import "./Nav.css";
 class Nav extends Component {
 
   render() {
-    const {breadcrumb, children, searchActive} = this.props;
+    const {breadcrumb, children, searchActive, visible} = this.props;
     return (
-      <nav className="nav">
-        <div>
-          <a className="logo" href="/">
-            <span className="data">Data</span> <span className="africa">Africa</span>
-          </a>
-          {
-            breadcrumb && breadcrumb.length ? breadcrumb.map((crumb, i) =>
-              i < breadcrumb.length - 1
-              ? <span><a className="link" href={`/profile/${crumb.id}`}>{ crumb.name }</a><span className="divider">/</span></span>
-              : <span className={searchActive ? "profile link active" : "profile link"} onClick={ this.props.activateSearch }>{ crumb.name }</span>
-            ) : null
-          }
-          { children }
-        </div>
-        <div>
-          <span className={searchActive ? "link active" : "link"} onClick={ this.props.activateSearch }>Search</span>
-          <a className="link" href="/">Map</a>
+      <nav className={ children ? visible ? "nav-container subnav" : "nav-container subnav hidden" : "nav-container" }>
+        <div className="nav">
+          <div>
+            <a className="logo" href="/">
+              <span className="data">Data</span> <span className="africa">Africa</span>
+            </a>
+            {
+              breadcrumb && breadcrumb.length ? breadcrumb.map((crumb, i) =>
+                i < breadcrumb.length - 1
+                ? <span key={ crumb.id }><a className="link" href={`/profile/${crumb.id}`}>{ crumb.name }</a><span className="divider">/</span></span>
+                : <span key={ crumb.id } className={searchActive ? "profile link active" : "profile link"} onClick={ this.props.activateSearch }>{ crumb.name }</span>
+              ) : null
+            }
+            { children }
+          </div>
+          <div>
+            <span className={searchActive ? "link active" : "link"} onClick={ this.props.activateSearch }>Search</span>
+            <a className="link" href="/">Map</a>
+          </div>
         </div>
       </nav>
     );
   }
 }
+
+Nav.defaultProps = {visible: true};
 
 export default connect(state => ({
   attrs: state.attrs.geo,
