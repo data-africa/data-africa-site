fd17377fd945efa691e86449913cfe579a1c1aa0
Jonathan Speiser <jspeis@gmail.com>
2017-03-14 20:18:28 -0400

    only show 20% cv variability

--- app/pages/Profile/climate/RainfallCV.jsx
+++ app/pages/Profile/climate/RainfallCV.jsx
@@ -8,24 +8,14 @@ import {COLORS_RAINFALL} from "helpers/colors";
 import {VARIABLES} from "helpers/formatters";
 
 import {API} from ".env";
-import Selector from "pages/Profile/ui/Selector";
 
 class RainfallCV extends SectionRows {
-  constructor(props) {
-    super(props);
-    this.state = {cvLevel: "cropland_rainfallCVgt20pct_pct"};
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(event) {
-    this.setState({cvLevel: event.target.value});
-  }
 
   render() {
     const {attrs, focus, profile} = this.props;
-    const {cvLevel} = this.state;
+    const cvLevel = "cropland_rainfallCVgt20pct_pct";
     const data = this.context.data.rainfallCv;
-    const hasData = data && data.length > 0 && data[0][cvLevel] !== undefined;
+    const hasData = data && data.length > 0 && (data[0][cvLevel] !== null && data[0][cvLevel] !== undefined);
 
     const sumlevel = profile.id.slice(0, 3);
     const adm0 = profile.id.slice(5, 10);
@@ -36,17 +26,13 @@ class RainfallCV extends SectionRows {
     const isAdm0 = profile.level === "adm0";
     const topoFilt = isAdm0 ? d => d : d => adm0 === d.properties.geo.slice(5, 10);
     const topoPath = isAdm0 ? "/topojson/continent.json" : "/topojson/cell5m/adm1.json";
-    const statValue = hasData ?  data[0][cvLevel] : null;
-    const pct = cvLevel === "cropland_rainfallCVgt20pct_pct" ? "20" : "30";
+    const statValue = hasData ? data[0][cvLevel] : null;
     const sentence = hasData ? <article>From {res.start_year} to {res.year}, {VARIABLES.cropland_cv(statValue)} of cropland area
-            in {res.geo_name} had rainfall variability greater than {pct}%.</article> : "";
+            in {res.geo_name} had rainfall variability greater than 20%.</article> : "";
 
-    const opts = [{label: "20% Rainfall Variability Threshold", value: "cropland_rainfallCVgt20pct_pct"},
-                  {label: "30% Rainfall Variability Threshold", value: "cropland_rainfallCVgt30pct_pct"}];
     return (
       <SectionRows>
         <SectionTitle>Rainfall Variability</SectionTitle>
-        <Selector options={opts} callback={this.onChange}/>
         {sentence}
         <SectionColumns>
           <Geomap config={{
