57bcc15fe7ccaa9d9f38021aeca9ba0313027e21
Jonathan Speiser <jspeis@gmail.com>
2017-03-27 15:51:30 -0400

    adds background content for about page

--- app/pages/About.jsx
+++ app/pages/About.jsx
@@ -86,10 +86,30 @@ class About extends Component {
         <TopicTitle slug="background">Background</TopicTitle>
         <section className="section">
           <p className="paragraph">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla hendrerit, lorem id mollis congue, nisl neque sollicitudin orci, at condimentum diam sem vitae purus. Curabitur ipsum urna, volutpat vitae lorem et, sagittis posuere tortor. Nulla semper tempor tellus, in convallis tellus euismod vitae. Integer molestie ultricies massa vitae facilisis. Praesent vitae sem vitae massa luctus iaculis. Nulla aliquet, urna sed condimentum ornare, lectus lacus rhoncus massa, eu scelerisque augue velit eu purus. Nam eget imperdiet mi. Praesent varius sapien purus, eu tempus nulla tincidunt ac. Nulla urna nisl, dapibus vel tincidunt non, ullamcorper eu diam.
+            Data Africa is an open-source platform designed to provide information
+            on key themes such as: health, agriculture, climate, and poverty across Africa. The goal of the site
+            is to present to wide audience easy to understand visual narratives related to the key themes.
           </p>
           <p className="paragraph">
-            Praesent tempor diam sit amet felis viverra tempus. Integer rhoncus ligula hendrerit, vehicula nibh et, lacinia leo. Morbi posuere nisi at ultricies auctor. Proin vitae tincidunt tortor. Vivamus elementum dictum ex vel tempor. Suspendisse ut facilisis eros, nec egestas orci. Mauris luctus elit justo, sit amet sollicitudin ante tincidunt quis. Ut iaculis facilisis magna, at scelerisque ipsum commodo id. Ut sed dui eu tellus fermentum mollis vitae a dolor. Phasellus sed nibh lacus. Fusce auctor magna vitae quam iaculis, vel condimentum dui vehicula. Integer cursus sapien vel imperdiet vestibulum. Nunc id felis vel eros fringilla bibendum ac sit amet elit. Sed viverra ac lorem non dapibus. Nullam dignissim arcu non est interdum hendrerit. Morbi a urna id ligula aliquet posuere.
+            The platform is initially focused around state and province level data for 13 countries, including:
+            <ol className="paragraph">
+              <li>Burkina Faso</li>
+              <li>Ethiopia</li>
+              <li>Ghana</li>
+              <li>Kenya</li>
+              <li>Malawi</li>
+              <li>Mali</li>
+              <li>Mozambique</li>
+              <li>Nigeria</li>
+              <li>Rwanda</li>
+              <li>Senegal</li>
+              <li>Tanzania</li>
+              <li>Uganda</li>
+              <li>Zambia</li>
+            </ol>
+          </p>
+          <p className="paragraph">
+          Over time, we anticipate expanding the coverage of the platform in terms of the number of countries covered as well as increasing the amount of data available through the platform. For any questions, comments, or more information on the site please contact <a href="mailto:hello@dataafrica.io" className="email">hello@dataafrica.io</a>.
           </p>
         </section>
 
