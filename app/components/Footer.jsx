bc202aa719447239b4cd9807055d68cd1e9f31de
Jonathan Speiser <jspeis@gmail.com>
2017-03-30 14:29:21 -0400

    makes links in footer clickable

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -23,8 +23,8 @@ class Footer extends Component {
             </ul>
           </div>
           <div className="logos">
-            <img className="ifpri" src="/images/logos/ifpri.png" />
-            <img className="datawheel" src={ `/images/logos/datawheel${ dark ? "-white" : "" }.png` } />
+            <a href="http://www.ifpri.org" target="_blank"><img className="ifpri" src="/images/logos/ifpri.png" /></a>
+            <a href="http://datawheel.us" target="_blank"><img className="datawheel" src={ `/images/logos/datawheel${ dark ? "-white" : "" }.png` } /></a>
           </div>
         </div>
       </footer>
