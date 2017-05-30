2dd04a9d1f8fed098cbebbf9defdd6378df21b9c
Dave Landry <landry.dave@gmail.com>
2017-05-30 16:59:02 -0400

    updates canon and removes switches rgba values to hex (closes #108)

--- app/helpers/colors.js
+++ app/helpers/colors.js
@@ -12,7 +12,32 @@ export const COLORS_CROP = {
 
 export const COLORS_GENDER = {
   female: "#9066BD",
-  male: "#FFC028"
+  female_severe: "#9066BD",
+  female_moderate: "#c8b3de",
+  male: "#FFC028",
+  male_severe: "#FFC028",
+  male_moderate: "#ffe094"
+};
+
+export const COLORS_RESIDENCE = {
+  rural: "#66BC05",
+  rural_severe: "#66BC05",
+  rural_moderate: "#b3de82",
+  urban: "#1B6DBF",
+  urban_severe: "#1B6DBF",
+  urban_moderate: "#8db6df"
+};
+
+export const COLORS_CONDITION = {
+  stunted: "#734FBA",
+  stunted_severe: "#734FBA",
+  stunted_moderate: "#b9a7dd",
+  wasted: "#1B6DBF",
+  wasted_severe: "#1B6DBF",
+  wasted_moderate: "#8db6df",
+  underweight: "#1BC1BA",
+  underweight_severe: "#1BC1BA",
+  underweight_moderate: "#8de0dd"
 };
 
 export const COLORS_POVERTY = {
@@ -21,25 +46,14 @@ export const COLORS_POVERTY = {
   sevpov: "#1BC1BA"
 };
 
-export const COLORS_RESIDENCE = {
-  rural: "#66BC05",
-  urban: "#1B6DBF"
-};
-
-export const COLORS_CONDITION = {
-  stunted: "#734FBA",
-  wasted: "#1B6DBF",
-  underweight: "#1BC1BA"
-};
-
 export const COLORS_RAINFALL = [
-  "rgba(20, 120, 190, 0.2)",
-  "rgba(20, 120, 190, 0.3)",
-  "rgba(20, 120, 190, 0.4)",
-  "rgba(20, 120, 190, 0.5)",
-  "rgba(20, 120, 190, 0.6)",
-  "rgba(20, 120, 190, 0.7)",
-  "rgba(20, 120, 190, 0.8)",
-  "rgba(20, 120, 190, 0.9)",
-  "rgb(20, 120, 190)"
+  "#D0E4F2",
+  "#B9D7EC",
+  "#A1C9E5",
+  "#8ABCDF",
+  "#72AED8",
+  "#5BA1D2",
+  "#4393CB",
+  "#2B86C5",
+  "#1478BE"
 ];
