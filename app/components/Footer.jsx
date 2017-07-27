3d9152a3865123c2e6fb76aa537a5eee54ad7b5e
Dave Landry <landry.dave@gmail.com>
2017-07-27 15:52:01 -0400

    updates footer logos

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -24,7 +24,8 @@ class Footer extends Component {
             </ul>
           </div>
           <div className="logos">
-            <a href="http://www.ifpri.org" target="_blank"><img className="ifpri" src="/images/logos/ifpri.png" /></a>
+            <a href="https://harvestchoice.org/" target="_blank"><img className="harvest-choice" src={ `/images/logos/harvest-choice${ dark ? "-white" : "" }.jpg` } /></a>
+            <a href="http://www.ifpri.org" target="_blank"><img className="ifpri" src={ `/images/logos/ifpri${ dark ? "-white" : "" }.png` } /></a>
             <a href="http://datawheel.us" target="_blank"><img className="datawheel" src={ `/images/logos/datawheel${ dark ? "-white" : "" }.png` } /></a>
           </div>
         </div>
